c71b301992f776499637884188ea87483ddff25c
prince-konwea <kokodecontee123@gmail.com>
2023-11-18 14:15:59 +0100

    feat: create middleware for userinput validation

---
+++ middlewares/middlewareValidation.js
@@ -0,0 +1,14 @@
+const validation = (schema) => async (req,res, next) => {
+   const body = req.body;
+ try{
+
+    await schema.validate(body)
+    next()
+
+ }catch(error){
+    return res.status(400).json({error})
+ }
+  
+}
+
+module.exports = validation;
