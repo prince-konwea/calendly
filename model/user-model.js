90bdccac3851374aaca984d7e717d9367ba32fbd
prince-konwea <kokodecontee123@gmail.com>
2023-11-18 03:24:30 +0100

    chore: fix typo in user model

--- model/user-model.js
+++ model/user-model.js
@@ -1,7 +1,7 @@
 import {Schema, model} from "mongoose";
 
 const userSchema = new Schema({
-    firstname: {type: Schema.Types.String},
+    firstName: {type: Schema.Types.String},
     lastName: {type: Schema.Types.String},
     email:{type: Schema.Types.String},
     password: {type:Schema.Types.String}
